90319b9cd7935986e74e3547f1414af18b632a01
Dan Angelin <dan.angelin@hotmail.com>
2021-12-15 22:10:37 +0200

    add weight & height pokemon

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let pokemonTitle = document.querySelector('.pokemon_title h1');
 let pokemonIcon = document.querySelector('.pokemon_icon');
 let pokemonDisplay = document.querySelector('#pokemon-display');
 let hp = document.querySelector('.hp h2');
+let weight = document.querySelector('.details_small #weight');
+let height = document.querySelector('.details_small #height');
 
 async function getPokemon() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + Math.floor(Math.random() * 898));
@@ -13,6 +15,8 @@ async function getPokemon() {
         pokemonDisplay.innerHTML += `<img src="${pokemon.sprites.other.home.front_default}" >`;
         pokemonIcon.innerHTML += `<img src="${pokemon.sprites.other.home.front_default}" >`;
         hp.innerText += ` ${pokemon.stats[0].base_stat}`;
+        weight.innerText = `${pokemon.weight / 10} kg`;  // weight default is in hectograms
+        height.innerText = `${pokemon.height * 10} cm`; // height default is in decimetre
     } catch (error) {
         console.log(error);
     }
