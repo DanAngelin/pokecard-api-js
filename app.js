51ab1097adaa98aea6a2b1f6eebc6d038f0a7d0b
Dan Angelin <dan.angelin@hotmail.com>
2021-12-20 00:33:41 +0200

    add button search pokemon by ID or Name

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ let weightPokemon = document.querySelector('.details_small #weight');
 let heightPokemon = document.querySelector('.details_small #height');
 let power1 = document.querySelector('.power_1 h3');
 let power2 = document.querySelector('.power_2 h3');
-let typePokemon = document.querySelector('.hp');
+let typePokemon = document.querySelector('.icon-type');
 let effectPower1 = document.querySelector('.description_power .effectpower1');
 let effectPower2 = document.querySelector('.description_power .effectpower2');
 let card = document.querySelector('.card');
+let pokeCard = document.querySelector('.pokemon_card p');
 
 const cardColors = {
 	normal: '#A8A77A',
@@ -33,39 +34,50 @@ const cardColors = {
 	fairy: '#D685AD',
 };
 
-async function getPokemon() {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + Math.floor(Math.random() * 898));
+const getPokemon = async querry => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${querry}`);
+    if (response.status === 404) {
+        return alert('asdew');
+    }
     const pokemon = await response.json();
-    console.log(pokemon)
-    let {name, sprites: {other: {home: {front_default}}}, stats:[{base_stat}], weight, height, 
+    let {name, id, sprites: {other: {home: {front_default}}}, stats:[{base_stat}], weight, height, 
         abilities: [{ability: {name: first, url: detailFirstAbility}}, {ability: {name: second, url: detailSecondAbility}}], 
         types: [{type: {name: typesPokemon}}],
         } = pokemon;
     try {
-        pokemonName.innerText += `${name[0].toUpperCase() + name.slice(1)}`
-        pokemonTitle.innerText += `${name[0].toUpperCase() + name.slice(1)}`
-        pokemonDisplay.innerHTML += `<img src="${front_default}" >`;
-        pokemonIcon.innerHTML += `<img src="${front_default}" >`;
-        hp.innerText += ` ${base_stat}`;
-        weightPokemon.innerText = `${weight} kg`;  // weight default is in hectograms
+        pokemonName.innerText = `${name[0].toUpperCase() + name.slice(1)}`;
+        pokemonTitle.innerText = `${name[0].toUpperCase() + name.slice(1)}`;
+        pokeCard.innerText = `Pokemon Card #${id}`;
+        pokemonDisplay.innerHTML = `<img src="${front_default}" >`;
+        pokemonIcon.innerHTML = `<img src="${front_default}" >`;
+        typePokemon.innerHTML = `<img src="img/${typesPokemon}.png" >`;
+        hp.innerText = ` ${base_stat}`;
+        weightPokemon.innerText = `${weight / 10} kg`;  // weight default is in hectograms
         heightPokemon.innerText = `${height * 10} cm`; // height default is in decimetre
         power1.innerText = `${first.toUpperCase()}`;
         power2.innerText = `${second.toUpperCase()}`;
-        typePokemon.innerHTML += `<img src="img/${typesPokemon}.png" >`;
         const fetchDetailFirstAbility = await fetch(detailFirstAbility);
         const firstAbility = await fetchDetailFirstAbility.json();
-        console.log(firstAbility)
         let {effect_entries: [{language: {name: nameLang}, short_effect: short_effectDe}, {language: {name: nameLang2},short_effect: short_effectEn}]} = firstAbility;
         effectPower1.innerText = `${(short_effectEn && nameLang2 === "en") ? short_effectEn : short_effectDe}`;
-        console.log(nameLang, nameLang2);
         const fetchDetailSecondAbility = await fetch(detailSecondAbility);
         const secondAbility = await fetchDetailSecondAbility.json();
         let {effect_entries: [{short_effect: short_effectDe1}, {short_effect: short_effectEn2}]} = secondAbility;
         effectPower2.innerText = `${short_effectEn2 ? short_effectEn2 : short_effectDe1}`;
         card.style.backgroundColor = cardColors[typesPokemon];
     } catch (error) {
-        console.log(error);
+        console.log('error');
     }
 
 }
-getPokemon();
+
+
+const btnSearch = document.querySelector('.btnSearch');
+const idPokeApi = () => {
+let pokemonById = document.querySelector('.pokemonById');
+let idPoke = pokemonById.value.toLowerCase();
+return idPoke;
+}
+
+
+btnSearch.addEventListener('click', () => getPokemon(idPokeApi()));
